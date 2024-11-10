8b0bcd65bcdb5af31a673f31e4640db777a55a3a
Muhammmad Shahrukh Khan <91933455+mdsharuk@users.noreply.github.com>
2024-11-10 05:30:40 -0800

    Add files via upload

---
+++ react-admin-dashboard/src/components/settings/Profile.jsx
@@ -0,0 +1,30 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+import muhammadshahrukh from "../../../public/muhammadShahrukh.jpeg";
+
+const Profile = () => {
+  return (
+    <SettingSection icon={User} title={"Profile"}>
+      <div className="flex flex-col sm:flex-row items-center mb-6">
+        <img
+          src={muhammadshahrukh}
+          alt="Profile"
+          className="rounded-full w-20 h-20 object-cover mr-4"
+        />
+
+        <div>
+          <h3 className="text-lg font-semibold text-gray-100">
+            Muhammad Shahrukh Khan
+          </h3>
+          <p className="text-gray-400">khan@gmail.com</p>
+        </div>
+      </div>
+
+      <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
+        Edit Profile
+      </button>
+    </SettingSection>
+  );
+};
+
+export default Profile;
